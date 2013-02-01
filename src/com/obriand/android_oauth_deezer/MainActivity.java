05afca1727796935b9356f4b663acb43cf26aa5e
Olivier Briand <olivier.briand@orange.com>
2013-02-01 16:28:02 +0100

    Add permissions

--- src/com/obriand/android_oauth_deezer/MainActivity.java
+++ src/com/obriand/android_oauth_deezer/MainActivity.java
@@ -1,15 +1,33 @@
 package com.obriand.android_oauth_deezer;
 
+import com.deezer.sdk.DeezerConnect;
+import com.deezer.sdk.DeezerConnectImpl;
+import com.deezer.sdk.DeezerError;
+import com.deezer.sdk.DialogError;
+import com.deezer.sdk.DialogListener;
+import com.deezer.sdk.OAuthException;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	
+    /** Your app Deezer appId. */
+    public final static String APP_ID = "";
+    /** Permissions requested on Deezer accounts. */
+    private final static String[] PERMISSIONS = new String[] {};
+    /** DeezerConnect object used for auhtentification or request. */
+    private DeezerConnect deezerConnect = new DeezerConnectImpl( APP_ID );
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		init();
+	}
+	
+	public void init() {
 	}
 
 	@Override
@@ -18,5 +36,29 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+	
+    /** Handle DeezerConnect callbacks. */
+    private class MyDialogHandler implements DialogListener {
+	    @Override
+	    public void onComplete(final Bundle values) {
+	     
+	    }//met
+	    @Override
+	    public void onDeezerError(final DeezerError deezerError) {
+	     
+	    }//met
+	    @Override
+	    public void onError(final DialogError dialogError) {
+	     
+	    }//met
+	    @Override
+	    public void onCancel() {
+	     
+	    }//met
+	    @Override
+	    public void onOAuthException(OAuthException oAuthException) {
+	     
+	    }//met
+    }//inner class
 
 }
